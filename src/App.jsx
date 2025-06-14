6f603b3554f88d03f8b69d8e8fc4baf70206dc03
harshavardhan-macha <172028181+harshavardhan-macha@users.noreply.github.com>
2025-06-14 11:51:53 +0530

    removed the commit message

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="app-container">
-      {/* News/Content Reader Application will be developed here */}
+      
     </div>
   );
 }
