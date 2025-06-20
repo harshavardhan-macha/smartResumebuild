08338deb7403860e03cb47bccb8754610102d371
harshavardhan-macha <172028181+harshavardhan-macha@users.noreply.github.com>
2025-06-20 18:07:52 +0530

    removed the test file

--- src/Test.jsx
+++
@@ -1,9 +0,0 @@
-const Test = () => {
-    return (
-        <div>
-            <h1>Test Component</h1>
-            <p>This is a test component to verify the setup.</p>
-        </div>
-    );
-}
-export default Test;
